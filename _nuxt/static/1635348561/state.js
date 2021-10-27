c468cb3cfe24ab384a19ac5c2928cd7a692960b7
hitman00 <hitman00@users.noreply.github.com>
2021-10-27 15:29:22 +0000

    deploy: 1f2ffa2091fde5af52cfeda35eb7d3c19ae01214

---
+++ _nuxt/static/1635348561/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fbitcoin-chart\u002F_nuxt\u002Fstatic\u002F1635348561",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fbitcoin-chart\u002F",assetsPath:"\u002Fbitcoin-chart\u002F_nuxt\u002F",cdnURL:a}}}}(null));
